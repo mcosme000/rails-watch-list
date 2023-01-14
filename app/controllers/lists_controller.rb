cc7b2f1d2e9c4a460e43334a45e2e2553bed905e
Maria Cosmen <mcosme00@gmail.com>
2023-01-14 17:04:51 +0900

    Create image_url attribute for list and update index view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -21,9 +21,15 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :image_url)
   end
 end
