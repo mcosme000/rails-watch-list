09d63db85a9522838a993b3696cacd2c20dd56cf
Maria Cosmen <mcosme00@gmail.com>
2023-01-18 10:49:22 +0900

    Fix migration

--- db/migrate/20230114033936_create_bookmarks.rb
+++ db/migrate/20230114033936_create_bookmarks.rb
@@ -1,4 +1,4 @@
-git class CreateBookmarks < ActiveRecord::Migration[7.0]
+class CreateBookmarks < ActiveRecord::Migration[7.0]
   def change
     create_table :bookmarks do |t|
       t.string :comment
